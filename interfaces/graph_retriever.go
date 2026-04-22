b90a44944a145aecc75fb1a4f6a4467951804778
Piyush Daiya <pdaiya@gmail.com>
2026-04-22 18:53:27 -0400

    Deliverable F and cross-repo alignment: green tests

--- interfaces/graph_retriever.go
+++ interfaces/graph_retriever.go
@@ -2,17 +2,7 @@ package interfaces
 
 import "context"
 
-type RelationshipContextQuery struct {
-	MatchedListUID string
-	Program        string
-	EntityType     string
-}
-
-type RelationshipContextResult struct {
-	Summary      []string
-	Paths        []string
-	Neighborhood []map[string]any
-}
+type RelationshipContextQuery = RelationshipContextRequest
 
 type GraphRetriever interface {
 	RetrieveRelationshipContext(ctx context.Context, query RelationshipContextQuery) (RelationshipContextResult, error)
--- interfaces/relationship_context.go
+++ interfaces/relationship_context.go
@@ -4,7 +4,16 @@ import "context"
 
 type RelationshipContextRequest struct {
 	MatchedListUID string
+	Program        string
+	EntityType     string
+}
+
+type RelationshipContextResult struct {
+	MatchedListUID string
 	Depth          int
+	Summary        []string
+	Paths          []string
+	Neighborhood   []map[string]any
 }
 
 type RelationshipContextRetriever interface {
